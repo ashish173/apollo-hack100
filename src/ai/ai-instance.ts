c64956d5d04fc82e1d376df0f671e31a82ae5f10
Ashish Singh <aitashish173@gmail.com>
2025-06-01 22:30:08 +0530

    gemini fix

--- src/ai/ai-instance.ts
+++ src/ai/ai-instance.ts
@@ -5,7 +5,8 @@ export const ai = genkit({
   promptDir: './prompts',
   plugins: [
     googleAI({
-      apiKey: process.env.GOOGLE_GENAI_API_KEY,
+      // apiKey: process.env.GOOGLE_GENAI_API_KEY,
+      apiKey: 'AIzaSyAzrmKYIujxximaQNCDW8PMng2slmQYhnA',
     }),
   ],
   model: 'googleai/gemini-2.0-flash',
