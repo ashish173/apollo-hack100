94906900a5f62fe21a745c0a51ecf5dfd9e26132
Chandra Shekhar <chandradot99@gmail.com>
2025-09-16 10:52:36 +0530

    fetch key from env

--- functions/src/ai/ai-instance.ts
+++ functions/src/ai/ai-instance.ts
@@ -1,15 +1,11 @@
 import { genkit } from "genkit";
 import { googleAI } from "@genkit-ai/googleai";
-const { defineString } = require("firebase-functions/params");
-
-const geminiApiKey = defineString("GOOGLE_GENAI_API_KEY");
-const GEMINI_API_KEY = geminiApiKey.value();
 
 export const ai = genkit({
   promptDir: "./prompts",
   plugins: [
     googleAI({
-      apiKey: GEMINI_API_KEY,
+      apiKey: process.env.GOOGLE_GENAI_API_KEY,
     }),
   ],
   model: "googleai/gemini-2.0-flash",
